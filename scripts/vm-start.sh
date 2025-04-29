742345714205a873e4c6ef1eb53b8611a472c371
Joshua Lester <jlester3@ewu.edu>
2025-04-28 17:09:21 -0700

    save

--- scripts/vm-start.sh
+++ scripts/vm-start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 echo "Updating system packages..."
-sudo apt-mark hold linux-image-azure linux-headers-azure linux-modules-azure
+sudo apt-mark hold linux-image-azure linux-headers-azure
 sudo apt update && sudo apt upgrade -y
 
 echo "Installing Python and pip..."
@@ -9,11 +11,12 @@ sudo apt install -y python3 python3-pip git python3.10-venv
 
 echo "Cloning app repo..."
 git clone https://github.com/Joshua-Lester3/DL-storyteller.git
+cd DL-storyteller
 git reset --hard
 git pull
 
 echo "Installing Python dependencies..."
-cd DL-storyteller/src
+cd src
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
